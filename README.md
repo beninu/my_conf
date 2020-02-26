73aa669e60e37543a70cea9891b75d2c3e71f5f1
Weng, Cijie <wengcijie@kuaishou.com>
2020-02-26 23:00:55 +0800

    ADD: vimrc

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my_conf
+
+```shell
+ curl -fsSL https://raw.githubusercontent.com/beninu/my_conf/master/vim/install.sh | bash -x
+```
--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #
 
+# backup $.vimrc
+VIMRC=${HOME}/.vimrc
+if [ -f ${VIMRC} ];
+then
+  mv  ${VIMRC} ${VIMRC}.$(date +%s)
+fi
 
-
+curl -o ${VIMRC} https://raw.githubusercontent.com/beninu/my_conf/master/vim/vimrc
